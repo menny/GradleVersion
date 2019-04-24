0bd41c7306839021f11803e360b409e1056ce531
Menny Even-Danan <menny@evendanan.net>
2019-04-24 10:31:35 -0400

    Lib and plugin modules

---
+++ lib/src/main/kotlin/net/evendanan/versiongenerator/generators/StaticVersionGenerator.kt
@@ -0,0 +1,19 @@
+@file:JvmName("StaticVersionGenerator")
+
+package net.evendanan.versiongenerator.generators
+
+import net.evendanan.versiongenerator.GenerationData
+import net.evendanan.versiongenerator.VersionGenerator
+
+class StaticVersionGenerator(private val staticVersionCode: Int)
+    : VersionGenerator("StaticVersionGenerator") {
+    constructor() : this(1)
+
+    override fun getVersionCode(generationData: GenerationData): Int {
+        return staticVersionCode
+    }
+
+    override fun isValidForEnvironment(): Boolean {
+        return true
+    }
+}
